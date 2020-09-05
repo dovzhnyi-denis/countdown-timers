9c906d155a8f6622a80523857f64c2c0f2126d82
Dovzhnyi Denys <dovzhny.denis@gmail.com>
2020-09-05 21:43:53 +0300

    v 0.32.0. Changed countdown time algorithm to be more accurate.

--- README.md
+++ README.md
@@ -6,8 +6,9 @@ Installation and running instruction:
   <li>Clone repository with <code style="background:lightgrey">git clone https://github.com/dovzhnyi-denis/countdown-timers.git</code></li>
   <li>Enter project directory;</li>
   <li>Execute commands:
-  <ul>  
+  <ul>
     <li><code style="background:lightgrey">npm i</code>;</li>
+    <li><code style="background:lightgrey">npm run build</code>;</li>
     <li><code style="background:lightgrey">npx serve -s build</code>;</li>
   </ul>
   <li>In browser visit <i>http://localhost:5000</i>, 5000 is the default port used by <b>serve</b>.</li>
@@ -17,8 +18,8 @@ Features:
 <ul>
   <li>Countdown timer can be added with name and custom date, both fields are required, unique name is required;</li>
   <li>After clicking Add Countdown a new countdown timer will appear with name and years/months/days/hours/minutes/seconds left to the specified date;</li>
-  <li>For extra visualization there will be a filling circle filling around each item, except for years;</li>
-  <li>Once the countdown reaches 0 it will be automatically removed;</li>
+  <li>For extra visualization there will be a filling circle, filling around each item, except for years;</li>
+  <li>Once the countdown reaches 0 its background will turn red(ish);</li>
   <li>Countdown can be removed by clicking on "X" in the top-right corner of the countdown element;</li>
   <li>There is no limit on how many countdowns can be added;</li>
   <li>Countdowns are stored in Web Storage "localstorage" object;</li>
--- src/components/countdown.js
+++ src/components/countdown.js
@@ -28,32 +28,130 @@ export default class Countdown extends React.Component {
     // calculate time left to endDate
     let timeLeft = endDate - new Date().getTime();
     // update state as long as time left is greater than 0,
-    // otherwise remove countdown
+    // otherwise set background to red (#e25a5c)
     if (timeLeft > 0) {
       this.setState(prevState => ({
         timeLeft: timeLeft 
       }));
-    } else this.props.remove(this.state.name);
+
+    } else {
+      this.setState(prevState => ({
+        timeLeft: 0
+      }));
+      this.countdownRow.style.backgroundColor = "#e25a5c";
+    }
   }
 
-  millisecToDate = timeLeft => {
-    let tLeft = timeLeft;
-    this.years = Math.floor(tLeft / 31104000000);
-    tLeft -= this.years * 31104000000;
-    this.months = Math.floor(tLeft / 2592000000);
-    tLeft -= this.months * 2592000000;
-    this.days = Math.floor(tLeft / 86400000);
-    tLeft -= this.days * 86400000;
-    this.hours = Math.floor(tLeft / 3600000);
-    tLeft -= this.hours * 3600000;
-    this.min = Math.floor(tLeft / 60000);
-    tLeft -= this.min * 60000;
-    this.sec = Math.floor(tLeft / 1000);
+//  millisecToDate = timeLeft => {
+//    let tLeft = timeLeft;
+//    this.years = Math.floor(tLeft / 31104000000);
+//    tLeft -= this.years * 31104000000;
+//    this.months = Math.floor(tLeft / 2592000000);
+//    tLeft -= this.months * 2592000000;
+//    this.days = Math.floor(tLeft / 86400000);
+//    tLeft -= this.days * 86400000;
+//    this.hours = Math.floor(tLeft / 3600000);
+//    tLeft -= this.hours * 3600000;
+//    this.min = Math.floor(tLeft / 60000);
+//    tLeft -= this.min * 60000;
+//    this.sec = Math.floor(tLeft / 1000);
+//  }
+
+// check if "year" is a leap year
+  checkLeapYear(year) {
+  // each month days in milliseconds
+  const monthDaysMS = [2678400000, 2419200000, 2678400000, 2592000000, 2678400000, 2592000000, 2678400000, 2678400000, 2592000000, 2678400000, 2592000000, 2678400000];
+    
+  if (!(year % 400)) {
+    // add one day to february
+    monthDaysMS[1] += monthDaysMS[1] / 28;
+    return monthDaysMS;
+  } else if (!(year % 100)) {
+    return monthDaysMS;
+  } else if (!(year % 4)) {
+    //add one day to february
+    monthDaysMS[1] += monthDaysMS[1] / 28;
+    return monthDaysMS;
   }
+  return monthDaysMS;
+}
+
+  yearsMonths(tLeft, endDate, curDate)  {
+    const endYear = endDate.getFullYear(); 
+    let curYear = curDate.getFullYear(),
+      curMonth = curDate.getMonth(),
+      month = 0;
+    
+    for (;curYear <= endYear; ++curYear) {
+      // array of curent year months days in milliseconds
+      const monthDaysMS = this.checkLeapYear(curYear);
+      let lastMonth = 12;
+      
+      if (curYear === endYear) lastMonth = endDate.getMonth();
+      for (;curMonth < lastMonth && tLeft.ms > monthDaysMS[curMonth]; ++curMonth) { 
+        tLeft.ms -= monthDaysMS[curMonth];
+        ++month;
+      }
+      curMonth = 0;
+    }
+
+    tLeft.years = Math.floor(month / 12);
+    month -= tLeft.years * 12;
+    tLeft.months = month;
+  }
+
+  days(tLeft, CYMDays, EYMDays, curDate, endDate, dayMS) {
+    let days = 0,
+      cMonthDays = CYMDays[curDate.getMonth()] / dayMS,
+      eMonthDays = EYMDays[endDate.getMonth()] / dayMS;
+  
+    for (let q = curDate.getDate(); q < cMonthDays && tLeft.ms > dayMS;
+      ++q) {
+      ++days;
+      tLeft.ms -= dayMS;
+    }
+  
+    for (let q = 1; q < eMonthDays && tLeft.ms > dayMS; ++q) {
+      ++days;
+      tLeft.ms -= dayMS;
+    }
+  
+    tLeft.days = days;
+  }
+
+  millisecToDate = ms => {
+  const tLeft = {ms},
+    endDate = new Date(tLeft.ms + new Date().getTime()),
+    curDate = new Date(),
+    // one minute in milliseconds
+    minMS = 60000,
+    // one hour in milliseconds
+    hourMS = minMS * 60,
+    // one day in milliseconds
+    dayMS = hourMS * 24;
+  // array of current year's month days in milliseconds
+  const CYMDays = this.checkLeapYear(curDate.getFullYear());
+  // array of end year's month days in milliseconds
+  const EYMDays = this.checkLeapYear(endDate.getFullYear());
+
+  this.yearsMonths(tLeft, endDate, curDate);
+
+  this.days(tLeft, CYMDays, EYMDays, curDate, endDate, dayMS);
+
+  tLeft.hours = Math.floor(tLeft.ms / hourMS);
+  tLeft.ms -= tLeft.hours * hourMS;
+
+  tLeft.min = Math.floor(tLeft.ms / minMS);
+  tLeft.ms -= tLeft.min * minMS;
+
+  tLeft.sec = Math.floor(tLeft.ms / 1000);
+
+  return tLeft;
+}
 
   render() {
   const { name, timeLeft} = this.state;
-  this.millisecToDate(timeLeft);
+  const tLeft = this.millisecToDate(timeLeft);
   // Mapping the date values to radius values
   const monthsRadius = mapNumber(this.months, 12, 0, 0, 360);
   const daysRadius = mapNumber(this.days, 30, 0, 0, 360);
@@ -62,39 +160,41 @@ export default class Countdown extends React.Component {
   const secondsRadius = mapNumber(this.sec, 60, 0, 0, 360);
 
     return (
-      <Row className="d-flex justify-content-center">
+      <Row className="d-flex justify-content-center"
+        ref={row => this.countdownRow = row}
+      >
         <Col xs={4} className="d-flex align-items-center 
           justify-content-center name"
         >
           <p>{name}</p>
         </Col>
         <Col className="countdown-item">
-          <p>{this.years}</p>
+          <p>{tLeft.years}</p>
           <span>YEARS</span>
         </Col>
         <Col className="countdown-item">
           <SVGCircle radius={monthsRadius} />
-          <p>{this.months}</p>
+          <p>{tLeft.months}</p>
           <span>MONTHS</span>
         </Col>
         <Col className="countdown-item">
           <SVGCircle radius={daysRadius} />
-          <p>{this.days}</p>
+          <p>{tLeft.days}</p>
           <span>DAYS</span>
         </Col>
         <Col className="countdown-item">
           <SVGCircle radius={hoursRadius} />
-          <p>{this.hours}</p>
+          <p>{tLeft.hours}</p>
           <span>HOURS</span>
         </Col>
         <Col className="countdown-item">
           <SVGCircle radius={minutesRadius} />
-          <p>{this.min}</p>
+          <p>{tLeft.min}</p>
           <span>MINUTES</span>
         </Col>
         <Col className="countdown-item">
           <SVGCircle radius={secondsRadius} />
-          <p>{this.sec}</p>
+          <p>{tLeft.sec}</p>
           <span>SECONDS</span>
         </Col>
         <Col xs={1}>
